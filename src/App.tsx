49fbdda9fbb1a363dea7e0d58303ec739ef1bb7f
Eason0628 <zhaowintoo@gmail.com>
2026-01-15 21:56:45 +0800

    15.1 temp

--- src/App.tsx
+++ src/App.tsx
@@ -6,32 +6,40 @@ import { useEffect } from "react";
 import { useSelector } from "react-redux";
 import { createBrowserRouter } from "react-router-dom";
 import { useState, Suspense } from "react";
+import { getMenu } from "./api/users";
+import { setMenus } from './store/login/authSlice';
+import { useDispatch } from 'react-redux';
 function App() {
   const { menuList } = useSelector((state: any) => state.authSlice);
   const [routes, setRoutes] = useState<any>(null);
+  const dispatch = useDispatch();
 
   useEffect(() => {
-    let routeList = generatesRoutes(menuList);
-    let routes = [...initRoutes]
-    routes[0].children = routeList
-    if (routes[0].children[0]) {
-      // 第一个路由的子路由设置为默认路由
-      routes[0].children[0].index = true
-    }
+    async function loadData() {
+      // sessionStorage.setItem("token", "mocktoken123456admin");
+      const { data } = await getMenu();
+      console.log('getMenu', data)
+      dispatch(setMenus(data))
+      const routes = generatesRoutes(data)//动态创建的路由表
+      const myRoutes = [...initRoutes];
+      myRoutes[0].children = routes;
+      myRoutes[0].children[0].index = true;
 
-    const browserRouters = createBrowserRouter(routes);
-    setRoutes(browserRouters);
-  }, [menuList])
+      const routeList = createBrowserRouter(myRoutes)
+      setRoutes(routeList);
+    }
+    loadData()
+  }, [])
 
   if (routes) {
     return (
       // Suspense作用是当子组件是懒加载时，显示fallback中的内容，当子组件加载完成后，显示子组件
-      <Suspense fallback={<div>loading</div>}>
+      <Suspense fallback={<div>loading..</div>}>
         <RouterProvider router={routes} />
       </Suspense>
     );
   } else {
-    return <div>loading</div>;
+    return <div>loading...</div>;
   }
 
 }
